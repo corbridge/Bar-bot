f1fcfccf78820ba26ec038b8c098c5e3ff401f10
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-04-20 17:25:08 -0600

    make two fonts in the same display

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 
 U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
 
+void drawTitle();
+
 void setup() {
   pinMode(motor_1, OUTPUT);
   pinMode(motor_2, OUTPUT);
@@ -27,8 +29,7 @@ void setup() {
   pinMode(motor_5, OUTPUT);
 
   u8g2.begin();
-  u8g2.enableUTF8Print();
-  u8g2.setFont(u8g2_font_profont11_tf); 
+  u8g2.enableUTF8Print(); 
   u8g2.setColorIndex(1); 
 }
 
@@ -36,7 +37,19 @@ void loop() {
   u8g2.firstPage();
     do
     {   
-      u8g2.drawTriangle(0,30, 0,40, 5,35);
-      u8g2.drawStr(10, 39, "HOla");
+      // u8g2.drawTriangle(0,30, 0,40, 5,35);
+      // u8g2.drawStr(10, 39, "HOla");
+
+      // u8g2.drawStr(25, 15, "BAR-BOT");
+      drawTitle();
+      u8g2.setFont(u8g2_font_profont11_tf);
+      u8g2.drawStr(25, 40, "Hola");
+
     } while( u8g2.nextPage() );
+}
+
+void drawTitle()
+{
+  u8g2.setFont(u8g2_font_profont22_tf);
+  u8g2.drawStr(25, 15, "BAR-BOT");
 }
